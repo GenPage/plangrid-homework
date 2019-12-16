c08a1254e1f08ed25288f3278ee1b57adbec1e11
Dylan Page <genpage@pagefortress.com>
2019-12-16 18:32:38 -0500

    Setup logic, git repo files, and Dockerfile

---
+++ README.md
@@ -0,0 +1,8 @@
+# plangrid-homework
+Homework problem for plangrid DevOps/SRE position. I am using Flask for 
+the HTTP endpoint as it is a lightweight "micro" web framework perfect
+for this task.
+
+# Running the application
+
+# Testing
--- app.py
+++ app.py
@@ -1,12 +1,36 @@
+import logging
+import json
+import os
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello_world() -> str:
+    app.logger.debug(f'{request.date} - {request.url}')
+    try:
+        if request.headers["Accept"].lower() == "application/json":
+            return json.dump(f'{"message": "Hello, World"}')
+    except KeyError:
+        # The header was not present
+        pass
+    return f'<p>Hello, World</p>\n'
+
+
+def debug_mode():
+    if os.getenv('LOGLEVEL') == 'DEBUG':
+        return True
+    return False
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=debug_mode(), use_reloader=False)
+
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
